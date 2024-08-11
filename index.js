7893d31494af8368a408a1c427c65738dcb0b913
Adelina-hub <adelina.macarascu@gmail.com>
2024-08-11 22:38:01 +0300

    Am mai lucrat la tema

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const button = document.querySelector(".btn-modal");
-const backdrop = document.querySelector(".modal-backdrop");
-button.addEventListener("click", toggleModal);
-backdrop.addEventListener("click", (e) => {
+
+
+ const button = document.querySelector(".btn-modal");
+ const backdrop = document.querySelector(".modal-backdrop");
+ button.addEventListener("click", toggleModal);
+ backdrop.addEventListener("click", (e) => {
   const target = e.target.closest(".btn-close-modal");
   if (!target) {
     return;
   }
   toggleModal();
 });
-function toggleModal() {
+ function toggleModal() {
   backdrop.classList.toggle("is-open");
 }
-
